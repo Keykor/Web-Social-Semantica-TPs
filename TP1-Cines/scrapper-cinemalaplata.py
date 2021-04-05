a86f9c89164c345b41852927bc4dfa1b6d4e31ff
unknown <lozabonora@gmail.com>
2021-04-05 02:44:22 -0300

    Modifica parseo y refactoriza cinemalaplata

--- TP1-Cines/scrapper-cinemalaplata.py
+++ TP1-Cines/scrapper-cinemalaplata.py
@@ -10,53 +10,62 @@ if __name__ == "__main__":
     for movie in movies:
         link = movie.find('a').get('href')
         fullLink = "http://www.cinemalaplata.com/"+link
-        movieHTLM = requests.get(fullLink).text
-        movieSoup = bs4.BeautifulSoup(movieHTLM, "html.parser")
-        movieData = movieSoup.find_all('div', attrs={"class":"dropcap6"})
-        movieDict = {}
+        movie_HTLM = requests.get(fullLink).text
+        movie_Soup = bs4.BeautifulSoup(movie_HTLM, "html.parser")
+        movie_data = movie_Soup.find_all('div', attrs={"class":"dropcap6"})
+        movie_dict = {}
 
         #obtiene y parsea titulo de la película
-        movieDict["Titulo"] = movieSoup.find_all('div', attrs={"class":"post-container page-title"})[0].string.strip()
+        movie_dict["Titulo"] = movie_Soup.find_all('div', attrs={"class":"post-container page-title"})[0].string.strip()
         
         #parsea datos de la película
-        for data in movieData:
+        for data in movie_data:
             title = data.h4.string.strip()
             info = data.p.span.string
             if (title == "Actores") or (title == "Director"):
                 #divide por ", "
-                infoList = info.split(", ")
+                info_list = info.split(", ")
                 #en muchos casos el último elemento viene con punto o espacios
-                infoList[-1] = infoList[-1].replace('.','').strip()
-                movieDict[title] = infoList
+                info_list[-1] = info_list[-1].replace('.','').strip()
+                movie_dict[title] = info_list
             elif (title == "Duracion"):
                 #transforma el primero en numero
-                movieDict[title] = int(info.split(" ")[0])
+                movie_dict[title] = int(info.split(" ")[0])
             else:
-                movieDict[title]=info
+                movie_dict[title]=info
         
         #parsea cines con sus funciones y horarios
-        movieDict["Cines"] = []
-        cinesData = movieSoup.find_all('div', attrs={'id':"ctl00_cph_pnFunciones"})[0].find_all('div')
-        for cine in cinesData:
-            cineDict = {}
-            cineDict["Nombre"] = cine.h5.text.strip()
-            funcionesData = cine.p.find_all('span')
-            cineDict["Funciones"] = []
-            for funciones in funcionesData:
-                horaList = funciones.text.split(" ")
-                #elimina elementos innecesarios '' y '-' de la lista
-                horaList = list(filter(lambda x: (x != '' and x != '-'), horaList))
-                #en la lista generada el primer elemento es el idioma y los demás horarios
-                idioma = horaList.pop(0).replace(':','')
-                for hora in horaList:
-                    funcionDict = {}
-                    funcionDict["Idioma"] = idioma
-                    funcionDict["Hora"] = hora
-                    cineDict["Funciones"].append(funcionDict)
-                
-            movieDict["Cines"].append(cineDict)
+        cines_data = movie_Soup.find_all('div', attrs={'id':"ctl00_cph_pnFunciones"})
+        cines_data = cines_data[0].find_all('div')
 
-        movieList.append(movieDict)
+        movie_dict["Cines"] = []
+        for cine in cines_data:
+            cine_data_list = cine.h5.text.strip().split(" - ")
+            theater_type = cine_data_list[1].split()[-1]
+            shows_data = cine.p.find_all('span')
+
+            cine_dict = {}
+            cine_dict["Nombre"] = cine_data_list[0]
+
+            cine_dict["Funciones"] = []
+            for show in shows_data:
+                hours_list = show.text.split(" ")
+
+                #elimina elementos innecesarios '' y '-' de la lista
+                hours_list = list(filter(lambda x: (x != '' and x != '-'), hours_list))
+
+                #en la lista generada el primer elemento es el idioma y los demás horarios
+                language = hours_list.pop(0).replace(':','')
+
+                show_dict = {}
+                show_dict["Sala"] = theater_type
+                show_dict["Idioma"] = language
+                show_dict["Hora"] = hours_list
+                cine_dict["Funciones"].append(show_dict)
+                
+            movie_dict["Cines"].append(cine_dict)
+
+        movieList.append(movie_dict)
     
     #guarda los datos en un json
     with open("cinemalaplata.json","w",encoding="utf-8") as file:
