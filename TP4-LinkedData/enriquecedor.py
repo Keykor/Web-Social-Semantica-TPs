97942a34c6b0d35810fcef728ad3f0fd8a13ee45
unknown <lozabonora@gmail.com>
2021-05-24 17:45:19 -0300

    Finaliza enriquecedor de dataset

--- TP4-LinkedData/enriquecedor.py
+++ TP4-LinkedData/enriquecedor.py
@@ -1,24 +1,55 @@
 import requests
 import rdflib
-from rdflib.namespace import OWL
+import sys
+from rdflib.namespace import Namespace, OWL
 
-g = rdflib.Graph()
+DBO = Namespace("http://dbpedia.org/ontology/")
+FOAF = Namespace("http://xmlns.com/foaf/0.1/")
 
-#print(r.text)
-#result = g.parse("https://raw.githubusercontent.com/Keykor/Web-Social-Semantica-TPs/main/cohort.ttl", format="turtle")
-#result = g.parse("galgadot.ttl", format="turtle", encoding="utf-8")
-#g.parse(data=r.text, format="turtle", encoding="utf-8")
-g.parse("links.ttl", format="turtle", encoding="utf-8")
+def main():
+    if len(sys.argv) < 3:
+        print("Faltan argumentos")
+        return
 
-links = []
-for s, p, o in g.triples((None, OWL.sameAs, None)):
-    links.append(o)
+    g = rdflib.Graph()
 
-g.parse("dataset-original.ttl", format="turtle", encoding="utf-8")
+    #agrega al grafo los links
+    g.parse(sys.argv[2], format="turtle", encoding="utf-8")
 
-for link in links:
-    r = requests.get(link, headers={'Accept': 'text/turtle'})
-    g.parse(data=r.text, format="turtle", encoding="utf-8")
+    #me guardo los links de dbpedia de las tripletas
+    links = []
+    for s, p, o in g.triples((None, OWL.sameAs, None)):
+        links.append(o)
 
-with open("dataset-enriquecido.ttl","w",encoding="utf-8") as file:
-    file.write(g.serialize(format="turtle").decode("utf-8"))
+    #recorro los links para obtener el ttl de cada actor y agregar su info al grafo
+    for link in links:
+        r = requests.get(link, headers={'Accept': 'text/turtle'})
+        graph_actor = rdflib.Graph()
+        graph_actor.parse(data=r.text, format="turtle", encoding="utf-8")
+
+        #para agregar la página de Wikipedia
+        for triplet in graph_actor.triples((None, FOAF['isPrimaryTopicOf'], None)):
+            g.add(triplet)
+
+        #para agregar la fecha de nacimiento
+        for triplet in graph_actor.triples((None, DBO['birthDate'], None)):
+            g.add(triplet)
+
+        #para agregar las ocupaciones
+        for s, p, o in graph_actor.triples((None, DBO['occupation'], None)):
+            g.add((s, p, o))
+
+            #ocuppation tiene una uri donde los title son las diferentes ocupaciones de la persona
+            req = requests.get(o, headers={'Accept': 'text/turtle'})
+            graph_occupation = rdflib.Graph()
+            graph_occupation.parse(data=req.text, format="turtle", encoding="utf-8")
+            for triplet in graph_occupation.triples((None, DBO['title'], None)):
+                g.add(triplet)
+
+    #agrega al grafo el dataset del primer argumento
+    g.parse(sys.argv[1], format="turtle", encoding="utf-8")
+
+    print(g.serialize(format="turtle").decode("utf-8"))
+
+if __name__ == "__main__":
+    main()
