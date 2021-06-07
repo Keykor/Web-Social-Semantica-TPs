23e426300bd41c0e05b53bf5e49b6134ad9d1faa
unknown <lozabonora@gmail.com>
2021-06-07 18:16:40 -0300

    Termina script para buscar info en otros grafos

--- TP5-Sparql/obtenerPersonas.py
+++ TP5-Sparql/obtenerPersonas.py
@@ -1,8 +1,9 @@
 from SPARQLWrapper import SPARQLWrapper
 from SPARQLWrapper.Wrapper import TURTLE, RDFXML
-from rdflib.namespace import Namespace, RDF, OWL, RDFS
-from rdflib import Graph, Literal
+from rdflib.namespace import Namespace, RDF, OWL
+from rdflib import Graph
 import time
+import sys
 
 SCHEMA = Namespace('https://schema.org/')
 DBO = Namespace('http://dbpedia.org/ontology/')
@@ -30,12 +31,12 @@ def consultarWikidata(g, name):
 
     sparql.setQuery("""
         CONSTRUCT {
-        ?person ?predicate ?object
+            ?person ?predicate ?object
         }
         WHERE {
-        ?person wdt:P31|wdt:P270 wd:Q5 .
-        ?person ?label "%s" .
-        ?person ?predicate ?object .
+            ?person wdt:P31 wd:Q5 .
+            ?person ?label "%s" .
+            ?person ?predicate ?object .
         }
     """ % name['name'])
 
@@ -51,7 +52,6 @@ def consultarWikidata(g, name):
 
     for s, p, o in grafo.triples((None, WDT['P31'], WD['Q5'])):
         g.add((MY_ONTOLOGY[name['uri']], OWL.sameAs, s))
-        print(str(s))
 
     for s, p, o in grafo.triples((None, None, None)):
         g.add((s, p, o))
@@ -62,17 +62,14 @@ def consultarDBpedia(g, name):
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 
     sparql.setQuery("""
-        PREFIX dbo: <http://dbpedia.org/ontology/>
-        PREFIX schema: <http://schema.org/>
-
         CONSTRUCT {
-        ?person ?predicate ?object
+            ?person ?predicate ?object
         }
         WHERE {
-        ?person a dbo:Person .
-        ?person dbo:birthName ?name .
-        ?person ?predicate ?object .
-        FILTER regex(?name, "%s")
+            ?person a dbo:Person .
+            ?person dbo:birthName ?name .
+            ?person ?predicate ?object .
+            FILTER regex(?name, "%s")
         }
     """ % name['name'])
 
@@ -83,13 +80,11 @@ def consultarDBpedia(g, name):
     except:
         time.sleep(60)
         results = sparql.query().convert()
-        
+
     grafo = Graph().parse(data=results, format="turtle", encoding="utf-8")
 
     for s, p, o in grafo.triples((None, RDF.type, DBO['Person'])):
         g.add((MY_ONTOLOGY[name['uri']], OWL.sameAs, s))
-        print(str(s))
-        pass
 
     for s, p, o in grafo.triples((None, None, None)):
         g.add((s, p, o))
@@ -97,14 +92,22 @@ def consultarDBpedia(g, name):
     return
 
 def main():
+    if len(sys.argv) < 3:
+        print("Faltan argumentos")
+        return
+
     g = Graph()
-    g.parse('dataset-enriquecido.ttl', format="turtle", encoding="utf-8")
+
+    #agrega al grafo los links
+    g.parse(sys.argv[2], format="turtle", encoding="utf-8")
 
     names = obtenerNombres(g)
 
     for name in names:
         consultarWikidata(g, name)
         consultarDBpedia(g, name)
+
+    print(g.serialize(format="turtle").decode("utf-8"))
 
 if __name__ == "__main__":
     main()
